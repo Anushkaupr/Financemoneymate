ca8655ae46dadbe114ecf885f37a15594b4fa559
Anushkaupr <anushkaupreti7@gmail.com>
2026-02-14 13:05:56 +0530

    cloudinary config

---
+++ app/src/main/java/com/example/financemoneymate/utils/CloudinaryConfig.kt
@@ -0,0 +1,9 @@
+package com.example.financemoneymate.utils
+
+object CloudinaryConfig {
+    // Found on your main Dashboard
+    const val CLOUD_NAME = "dw9jwlsxo"
+
+    // Found in Settings -> Upload -> Upload Presets (MUST BE UNSIGNED)
+    const val UPLOAD_PRESET = "moneymate_preset"
+}
